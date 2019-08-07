6c894ab1eca1d4cbd20461bbc9771ff96fd81508
Hadar Romashkano <haromask@microsoft.com>
2019-08-07 17:36:26 +0300

    Handling 0 tournaments

--- BeachVolleyball.Server/ClientApp/src/components/Home.js
+++ BeachVolleyball.Server/ClientApp/src/components/Home.js
@@ -9,11 +9,7 @@ const Home = () => {
   const { tournamentsState } = useContext(TournamentsContext);
   return (
     <HomeContainer>
-      <p>
-        ברוכ\ה הבא\ה! רוצה לדעת נגד מי תשחק\י בבית בטורניר? בחר\י בטורניר
-        מהרשימה:
-      </p>
-      <TournamentsNav />
+      {!tournamentsState.loading && <TournamentsNav />}
       {tournamentsState.loading && <Loading />}
       {tournamentsState.error && (
         <p>אוף! לא הצלחתי למשוך את המידע מהשרת :( נסה\י לרענן את העמוד</p>
@@ -22,22 +18,35 @@ const Home = () => {
   );
 };
 
-const Loading = () => <div><p>טוען טורנירים...</p><Spinner animation="border" variant="info" /></div>;
+const Loading = () => (
+  <div>
+    <p>טוען טורנירים...</p>
+    <Spinner animation="border" variant="info" />
+  </div>
+);
 
 const TournamentsNav = () => {
   const { tournamentsState } = useContext(TournamentsContext);
   if (tournamentsState.tournaments.length === 0) {
-    return null;
+    return <p>אין אף טורניר בפתח, איזה באסה!</p>;
   }
 
   console.log(
     "Creating tournament links: " + tournamentsState.tournaments.length
   );
   const tourLinks = tournamentsState.tournaments.map(tour => (
-    <TournamentLink {...tour} key={tour.id}/> 
+    <TournamentLink {...tour} key={tour.id} />
   ));
 
-  return <Nav vertical>{tourLinks}</Nav>;
+  return (
+    <React.Fragment>
+      <p>
+        ברוכ\ה הבא\ה! רוצה לדעת נגד מי תשחק\י בבית בטורניר? בחר\י בטורניר
+        מהרשימה:
+      </p>
+      <Nav vertical>{tourLinks}</Nav>
+    </React.Fragment>
+  );
 };
 
 const HomeContainer = styled.div`
