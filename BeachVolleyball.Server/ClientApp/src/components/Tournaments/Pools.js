3a58a69ee0de568ddb0c49e308943413e73c1f1b
Hadar Romashkano <haromask@microsoft.com>
2019-08-11 16:12:47 +0300

    Added teams num in each category

--- BeachVolleyball.Server/ClientApp/src/components/Tournaments/Pools.js
+++ BeachVolleyball.Server/ClientApp/src/components/Tournaments/Pools.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 import Pool from "./Pool";
 import media from "../../utils/Media";
 
-const Pools = ({ pools }) => {
+const Pools = ({ pools, teamsNumber }) => {
   if (pools.length > 0) {
     const poolsViews = pools.map(pool => <Pool key={pool.number} {...pool} />);
-    return <PoolsContainer>{poolsViews}</PoolsContainer>;
+    return <div><CategoryTitle>מספר הקבוצות הרשומות בקטגוריה: {teamsNumber}</CategoryTitle><PoolsContainer>{poolsViews}</PoolsContainer></div>;
   }
   else {
     return <PoolsContainer><p>כמה עצוב, אף זוג עדיין לא נרשם לקטגוריה זו <span role="img" aria-label="sad emoji" style={{margin: "0px"}}>&#128577;</span></p></PoolsContainer>;
@@ -25,6 +25,10 @@ const PoolsContainer = styled.div`
       margin: 0rem 3rem;
     }
   `}
+`;
+
+const CategoryTitle = styled.p`
+  margin: 1rem;
 `;
 
 export default Pools;
